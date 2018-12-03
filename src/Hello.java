9ad345844f8803b128f0383cdd4917b5d435eb62
Jereu <jastudim@outlook.com>
2018-12-03 15:45:21 +0000

    add training  from the first part

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args){
-        printMegaBytesAndKiloBytes(3079);
+        //printMegaBytesAndKiloBytes(3079);;
     }
 
     public static void printMegaBytesAndKiloBytes(int kiloBytes){
@@ -12,4 +12,49 @@ public class Hello {
             System.out.println(kiloBytes + " KB = " +    value + " MB and " + remaining + " KB" );
         }
     }
+
+    public static boolean bark(boolean barking, int hourOfDay){
+        if(barking){
+            if(hourOfDay < 0 || hourOfDay > 23){
+                return false;
+            }else{
+                if(hourOfDay < 8 || hourOfDay > 22){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public static boolean isLeapYear(int year){
+        if(year >= 1 && year <= 9999){
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean hasEqualSum(int one, int two, int three){
+        if(one + two == three){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public static boolean hasTeen(int one, int two, int three){
+        if((one >= 13 && one <= 19) || (two >= 13 && two <= 19) || (three >= 13 && three <= 19)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
